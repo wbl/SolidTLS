901db1dde4d6060850992bf695cc54dcef0e1064
Watson Ladd <watsonbladd@gmail.com>
2014-06-30 21:47:37 -0700

    First chop at safe buffers

---
+++ src/util/buf.c
@@ -0,0 +1,108 @@
+#include<assert.h>
+#include<stdint.h>
+#include "buf.h"
+
+void
+tls_buf_init(tls_buf *t)
+{
+        t->c = NULL;
+        t->cap = 0;
+        t->len = 0;
+}
+
+uint8_t
+tls_buf_get(tls_buf t, size_t index)
+{
+        assert(index < t->len);
+        return t->c[index];
+}
+
+int
+tls_buf_set(tls_buf *t, size_t index, uint8_t value)
+{
+        uint8_t *space;
+        size_t oldcap = t->cap;
+        while (index >= t->cap) {
+                space = realloc(t->c, 2*t->cap+1);
+                if (space == NULL) {
+                        free(t->c);
+                        return -1;
+                }
+                t->cap = 2*t->cap+1;
+        }
+        for (size_t i = oldcap; i < t->cap; i++){
+                t->c[i] = 0;
+        }
+        if (t->len <= index) {
+                t->len = index+1;
+        }
+        t->c[index] = value;
+        return 0;
+}
+
+int
+tls_buf_clone (tls_buf *dst, tls_buf src)
+{
+        dst->cap = src->len;
+        dst->len = src->len;
+        dst->c = malloc(src->len);
+        if (dst->c == NULL) {
+                return -1;
+        }
+        memcpy(dst->c, src->c, src->len);
+        return 0;
+}
+
+int
+tls_buf_copy_string(tls_buf *dst, const char * src)
+{
+        free(dst->c);
+        dst->len = strlen(src);
+        dst->cap = dst->len;
+        dst->c = malloc(dst->cap);
+        if (dst->c == NULL) {
+                return -1;
+        }
+        for (size_t i = 0; src[i]; i++) {
+                dst->c[i]=src[i];
+        }
+        return 0;
+}
+
+size_t
+tls_buf_len(const tls_buf *t)
+{
+        return t->len;
+}
+
+int
+tls_buf_append(tls_buf *dst, tls_buf *src)
+{
+        return tls_buf_copy(dst, src, tls_buf_len(dst), 0, tls_buf_len(src));
+}
+
+
+void
+tls_buf_clean(tls_buf *t)
+{
+        free(t->c);
+        t->len = 0;
+        t->cap = 0;
+}
+
+int
+tls_buf_copy(tls_buf* dst, tls_buf* src, size_t dst_off, size_t src_off,
+             size_t len)
+{
+        /*
+         * The below is slow as molassas, but is safe.
+         */
+        uint8_t b;
+        for (size_t i = 0; i < len; i++) {
+                b = tls_buf_get(src, src_off+i);
+                if (tls_buf_set(dst, dst_off+i, b)) {
+                        return -1;
+                }
+        }
+        return 0;
+}
---
+++ src/util/buf.h
@@ -0,0 +1,16 @@
+typedef struct {
+  uint8_t *c;
+  size_t len;
+  size_t cap;
+}  tls_buf;
+
+void tls_buf_init(tls_buf* t);
+int tls_buf__copy_string(tls_buf* t, const char * str);
+uint8_t tls_buf_get(const tls_buf* t, size_t index);
+int tls_buf_set(tls_buf* t, size_t index, uint8_t value);
+int tls_buf_clone(tls_buf *dst, const tls_buf* src);
+int tls_buf_append(tls_buf *dst, const tls_buf* src);
+int tls_buf_copy(tls_buf *dst, const tls_buf* src, size_t dst_off,
+                     size_t src_off, size_t len);
+void tls_buf_clean(tls_buf *t);
+size_t tls_buf_len(tls_buf *t);
